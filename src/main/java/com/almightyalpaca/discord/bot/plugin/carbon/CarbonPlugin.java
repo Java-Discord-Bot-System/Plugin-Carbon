3f5fe307050f8bc606219a2d8e2116bd811e729c
Almighty-Alpaca <Almighty-Alpaca@users.noreply.github.com>
2016-04-08 23:32:25 +0200

    Updates to work with latest Core changes

--- src/main/java/com/almightyalpaca/discord/bot/plugin/carbon/CarbonPlugin.java
+++ src/main/java/com/almightyalpaca/discord/bot/plugin/carbon/CarbonPlugin.java
@@ -10,6 +10,7 @@ import com.almightyalpaca.discord.bot.system.exception.PluginLoadingException;
 import com.almightyalpaca.discord.bot.system.exception.PluginUnloadingException;
 import com.almightyalpaca.discord.bot.system.plugins.Plugin;
 import com.almightyalpaca.discord.bot.system.plugins.PluginInfo;
+import com.google.common.util.concurrent.MoreExecutors;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
@@ -18,11 +19,11 @@ import net.dv8tion.jda.events.guild.GuildLeaveEvent;
 
 public class CarbonPlugin extends Plugin {
 
-	private static final PluginInfo	INFO	= new PluginInfo("com.almightyalpaca.discord.bot.plugin.carbon", "1.0.0", "Almighty Alpaca", "Carbon Plugin", "Send bot statistics to carbonitex.com");
+	private static final PluginInfo INFO = new PluginInfo("com.almightyalpaca.discord.bot.plugin.carbon", "1.0.0", "Almighty Alpaca", "Carbon Plugin", "Send bot statistics to carbonitex.com");
 
-	Config							carbonConfig;
+	private Config carbonConfig;
 
-	ScheduledExecutorService		scheduledExecutor;
+	private ScheduledExecutorService scheduledExecutor;
 
 	public CarbonPlugin() {
 		super(CarbonPlugin.INFO);
@@ -30,30 +31,38 @@ public class CarbonPlugin extends Plugin {
 
 	@Override
 	public void load() throws PluginLoadingException {
-		this.carbonConfig = this.getBridge().getSecureConfig("carbonitex");
-		this.scheduledExecutor = Executors.newScheduledThreadPool(1, r -> new Thread(r, "Carbon Reporter Thread"));
+
+		this.carbonConfig = this.getSharedConfig("carbonitex");
+
+		if (carbonConfig.getString("key", "Your Key") == "Your Key") {
+			throw new PluginLoadingException("Pls add your carbonitex key to the config");
+		}
+
+		this.scheduledExecutor = Executors.newSingleThreadScheduledExecutor(r -> new Thread(r, "Carbon Reporter Thread"));
 
 		this.scheduledExecutor.scheduleAtFixedRate(() -> {
 			this.sendStats();
-		}, 0, 1, TimeUnit.HOURS);
+		} , 30, 60, TimeUnit.MINUTES);
 
 		this.registerEventHandler(this);
 	}
 
 	@EventHandler
 	public void onGuildJoinEvent(final GuildJoinEvent event) {
-		this.sendStats();
+		// Commented until JDA doesn't send it for guilds getting available
+		// this.sendStats();
 	}
 
 	@EventHandler
 	public void onGuildLeaveEvent(final GuildLeaveEvent event) {
-		this.sendStats();
+		// Commented until JDA doesn't send it for guilds getting available
+		// this.sendStats();
 	}
 
 	public void sendStats() {
 		try {
-			final String response = Unirest.post("https://www.carbonitex.net/discord/data/botdata.php").field("key", this.carbonConfig.getString("key")).field("servercount", this.getBridge().getJDA()
-					.getGuilds().size()).asString().getBody();
+			final String response = Unirest.post("https://www.carbonitex.net/discord/data/botdata.php").field("key", this.carbonConfig.getString("key"))
+				.field("servercount", this.getJDA().getGuilds().size()).asString().getBody();
 			System.out.println("Successfully posted the botdata to carbonitex.com: " + response);
 		} catch (final UnirestException e) {
 			System.out.println("An error occured while posting the botdata to carbonitex.com");
@@ -63,13 +72,6 @@ public class CarbonPlugin extends Plugin {
 
 	@Override
 	public void unload() throws PluginUnloadingException {
-		this.scheduledExecutor.shutdown();
-		try {
-			if (!this.scheduledExecutor.awaitTermination(10, TimeUnit.SECONDS)) {
-				this.scheduledExecutor.shutdownNow();
-			}
-		} catch (final InterruptedException e) {
-			e.printStackTrace();
-		}
+		MoreExecutors.shutdownAndAwaitTermination(scheduledExecutor, 10, TimeUnit.SECONDS);
 	}
 }
